12e03291ff7a0c800d6ff623b8fc726fca58d18f
JM Dev <juanma261005@gmail.com>
2024-10-29 15:02:20 -0300

    styles fix

--- loginTask/settings.py
+++ loginTask/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
